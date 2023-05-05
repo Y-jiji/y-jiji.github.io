a0acb96046d1c1e5cd61cd92d369f979cd1ac69f
Andy Yang <71013200+Y-jiji@users.noreply.github.com>
2023-05-05 16:47:44 +0800

    Add: initialized project structure of info-scanner

--- info-scanner/target/debug/deps/info_scanner-c80befe893ae2494.d
+++
@@ -1,5 +0,0 @@
-D:\project\blog\info-scanner\target\debug\deps\info_scanner-c80befe893ae2494.rmeta: src\main.rs
-
-D:\project\blog\info-scanner\target\debug\deps\info_scanner-c80befe893ae2494.d: src\main.rs
-
-src\main.rs:
