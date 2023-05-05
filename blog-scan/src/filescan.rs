55fe127f1eeb42d949220532fc86f7cab999be16
Andy Yang <71013200+Y-jiji@users.noreply.github.com>
2023-05-05 22:26:05 +0800

    Fix: eliminate warnings in blog-scan tool (minr)

--- blog-scan/src/filescan.rs
+++ blog-scan/src/filescan.rs
@@ -1,22 +1,22 @@
-use std::{path::PathBuf, time::{SystemTime, UNIX_EPOCH}};
 use walkdir::WalkDir;
-use chrono::{DateTime, Utc, TimeZone};
+use std::path::PathBuf;
+// use chrono::{DateTime, Utc, TimeZone};
 
-fn to_datatime(t: SystemTime) -> DateTime<Utc> {
-    let (sec, nsec) = match t.duration_since(UNIX_EPOCH) {
-        Ok(dur) => (dur.as_secs() as i64, dur.subsec_nanos()),
-        Err(e) => { // unlikely but should be handled
-            let dur = e.duration();
-            let (sec, nsec) = (dur.as_secs() as i64, dur.subsec_nanos());
-            if nsec == 0 {
-                (-sec, 0)
-            } else {
-                (-sec - 1, 1_000_000_000 - nsec)
-            }
-        },
-    };
-    Utc.timestamp_opt(sec, nsec).unwrap()
-}
+// fn to_datatime(t: SystemTime) -> DateTime<Utc> {
+//     let (sec, nsec) = match t.duration_since(UNIX_EPOCH) {
+//         Ok(dur) => (dur.as_secs() as i64, dur.subsec_nanos()),
+//         Err(e) => { // unlikely but should be handled
+//             let dur = e.duration();
+//             let (sec, nsec) = (dur.as_secs() as i64, dur.subsec_nanos());
+//             if nsec == 0 {
+//                 (-sec, 0)
+//             } else {
+//                 (-sec - 1, 1_000_000_000 - nsec)
+//             }
+//         },
+//     };
+//     Utc.timestamp_opt(sec, nsec).unwrap()
+// }
 
 pub fn scan(curr_dir: &PathBuf) -> Vec<PathBuf> {
     let post_dir = curr_dir.join(crate::POST);
