abbb40204992d7fb6c38000f00d06b333553f0c9
Andy Yang <71013200+Y-jiji@users.noreply.github.com>
2023-05-05 22:24:40 +0800

    Add: yaml front matter parser, make it work

--- blog-scan/src/main.rs
+++ blog-scan/src/main.rs
@@ -4,13 +4,23 @@ use std::str::FromStr;
 mod summary_writer;
 mod markdown_to_json;
 mod filescan;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use markdown_to_json::parse_markdown_front_matter;
+use serde::*;
 
 // the scanned post folder
 const POST: &str = "post";
 // the output folder
 const INFO: &str = "info";
+
+#[derive(Debug, Serialize, Deserialize, Default, Clone)]
+struct TopicItem {
+    abstr: Option<String>,
+    title: Option<String>,
+    ahref: Option<String>,
+    tag: Vec<String>,
+    date: Option<String>,
+}
 
 // a simple script for parsing markdown into json indices
 fn main() {
@@ -44,7 +54,9 @@ fn main() {
     let json_value = serde_json::to_value(meta_table.clone()).unwrap();
     std::fs::write(dir.join(INFO).join("table-meta.json"), json_value.to_string()).unwrap();
     // build full indices on meta table, put them to info folder as index-*.json
-    let mut json_index = HashMap::<String, HashMap<serde_yaml::Value, Vec<String>>>::new();
+    let mut json_index = HashMap::<String, HashMap<serde_yaml::Value, HashSet<String>>>::new();
+    // json for topic
+    let mut json_topic = HashMap::<String, Vec<TopicItem>>::new();
     for (file, value) in meta_table.iter() {
         match value {
             serde_yaml::Value::Mapping(mapping) => {
@@ -52,21 +64,38 @@ fn main() {
                 let mut has_title = false;
                 let mut has_summa = false;
                 let mut has_date  = false;
+                let mut topic_entry = (None::<String>, TopicItem::default());
+                topic_entry.1.ahref = Some(file.clone());
                 for (key, value) in mapping.into_iter() {
                     let key = key.as_str().unwrap().to_string();
                     if &key == "topic" { has_topic = true; }
                     if &key == "title" { has_title = true; }
                     if &key == "abstr" { has_summa = true; }
                     if &key == "date"  { has_date  = true; }
+                    if key == "topic" {
+                        topic_entry.0 = value.as_str().map(|s| s.to_string());
+                    } else if key == "abstr" {
+                        topic_entry.1.abstr = value.as_str().map(|s| s.to_string());
+                    } else if key == "title" {
+                        topic_entry.1.title = value.as_str().map(|s| s.to_string());
+                    } else if key == "date" {
+                        topic_entry.1.date = value.as_str().map(|s| s.to_string());
+                    } else if key == "tag" {
+                        match value.as_str() {
+                            Some(s) => topic_entry.1.tag.push(s.to_string()),
+                            None => {}
+                        };
+                    }
                     let mut for_each_entry = |key: &String, value: serde_yaml::Value, file: &String| {
                         json_index.entry(key.to_string())
-                            .and_modify(|e| {e.entry(value.clone()).and_modify(|e| e.push(file.to_string())).or_insert(vec![file.to_string()]); })
-                            .or_insert(HashMap::from([(value, vec![file.to_string()])]));
+                            .and_modify(|e| {e.entry(value.clone()).and_modify(|e| {e.insert(file.to_string());}).or_insert(HashSet::from([file.to_string()])); })
+                            .or_insert(HashMap::from([(value, HashSet::from([file.to_string()]))]));
                     };
                     let process_string = |s: &String| {
-                        s.split(|c: char| !(c.is_alphabetic()))
+                        s.split(|c: char| !(c.is_alphabetic() && c.is_alphanumeric()))
                             .filter(|x| x.len() > 0)
                             .map(|x| serde_yaml::Value::String(x.to_string()))
+                            .chain([serde_yaml::Value::String(s.to_string())])
                             .collect::<Vec<_>>()
                     };
                     match value {
@@ -97,6 +126,10 @@ fn main() {
                 }
                 if !has_topic {
                     println!("Warning: {file:?} doesn't have topic label, so we will not put it on the home page!");
+                } else {
+                    json_topic.entry(topic_entry.0.unwrap())
+                        .and_modify(|v| v.push(topic_entry.1.clone()))
+                        .or_insert(vec![topic_entry.1]);
                 }
                 if !has_summa {
                     println!("Warning: {file:?} doesn't have abstr label, so we will not generate an summary ont the home page");
@@ -113,5 +146,5 @@ fn main() {
             dir.join(INFO).join(format!("index-{index_name}.json")), 
             serde_json::to_value(index_file).unwrap().to_string()).unwrap();
     }
-    println!("");
+    std::fs::write(dir.join(INFO).join("topic-list.json"), serde_json::to_value(json_topic).unwrap().to_string()).unwrap();
 }
