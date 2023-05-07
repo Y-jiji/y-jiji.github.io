7637d2ea76d326d8432df6a94ad145fa5cf49b79
Andy Yang <71013200+Y-jiji@users.noreply.github.com>
2023-05-07 17:55:05 +0800

    Fix: overflowing image in post.html

--- site/config.js
+++ site/config.js
@@ -1,5 +1,5 @@
-// const root = "http://localhost:3000/"
-const root = "https://y-jiji.github.io/blog/"
+const root = "http://localhost:3000/"
+// const root = "https://y-jiji.github.io/blog/"
 
 /* the site to serve from */
 const site = root+"site/"
