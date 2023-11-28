2f6575e27c0de60915abbd90399ae19874cda4e1
y-jiji <y.jijiji.data.science@gmail.com>
2023-11-28 22:46:55 +0800

    2023_11_28

--- blog-scan/src/markdown_to_json.rs
+++ blog-scan/src/markdown_to_json.rs
@@ -4,7 +4,7 @@ pub fn parse_markdown_front_matter(file: String) -> Result<serde_yaml::Value, se
     let mut parse_option = ParseOptions::mdx();
     parse_option.constructs.frontmatter = true;
     let yaml_front_matter = 
-        to_mdast(&file, &parse_option).unwrap().children().unwrap().into_iter()
+        to_mdast(&file, &parse_option).expect(&file).children().unwrap().into_iter()
         .filter(|x| matches!(x, mdast::Node::Yaml(_)))
         .fold(String::new(), |x, y| x + &"\n" + &y.to_string());
     serde_yaml::from_str(&yaml_front_matter)
